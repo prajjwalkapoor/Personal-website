b466e223ed77cf1e40ef4ba3a90a91e4bb98ec18
Prajjwal kapoor <68912239+prajjwalkapoor@users.noreply.github.com>
2021-05-04 20:39:43 +0530

    feature: mobile menu

--- JS/nav.js
+++ JS/nav.js
@@ -8,10 +8,10 @@ const changeHamburger = () => {
 
   if (
     document.getElementById("hamburger").src ==
-    "http://127.0.0.1:5500/img/hamburger.svg"
+    "http://prajjwal.ml/img/hamburger.svg"
   ) {
     document.getElementById("hamburger").src =
-      "http://127.0.0.1:5500/img/hamburger-close.svg";
+      "http://prajjwal.ml/img/hamburger-close.svg";
   } else {
     document.getElementById("hamburger").src =
       "http://127.0.0.1:5500/img/hamburger.svg";

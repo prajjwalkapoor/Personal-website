dfb258db69a30750358712bba6481bf1b4fb3958
Prajjwal kapoor <68912239+prajjwalkapoor@users.noreply.github.com>
2021-05-05 16:31:25 +0530

    fix: mobile menu

--- JS/nav.js
+++ JS/nav.js
@@ -11,14 +11,15 @@ const changeHamburger = () => {
   }
 
   var mobNav = document.getElementById("mobile-nav");
-  if (mobNav.style.display === "flex") {
-    mobNav.style.display = "none";
-  } else if ((mobNav.style.display = "none")) {
-    mobNav.style.display = "flex";
-  }
 
+  if (mobNav.style.visibility === "visible") {
+    mobNav.style.visibility = "hidden";
+    mobNav.style.opacity = "0";
+  } else if ((mobNav.style.visibility = "hidden")) {
+    mobNav.style.visibility = "visible";
+    mobNav.style.opacity = "1";
+  }
   var hamburger = document.getElementById("hamburger");
-  console.log(hamburger.style.position);
   if (hamburger.style.position === "static") {
     hamburger.style.display = "fixed";
   } else if ((hamburger.style.display = "fixed")) {
